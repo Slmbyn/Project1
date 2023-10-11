e30aff37aa1e551a05684d692ab296871a789b3e
SelamB <selambeyene2@gmail.com>
2023-10-11 12:53:30 -0700

    fixes bugs

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let snake = [
 
 let direction = null;
 let autoMoveInterval = setInterval(autoMoveSnake, 100);
+const gameInterval = setInterval(gameloop, 50);
 let points = 0;
 let highPoints = 0;
 
@@ -18,22 +19,11 @@ const score = document.querySelector('.score');
 const highScore = document.querySelector('.highscore');
 
 
-
-
 /* ----- CANVAS SETUP ------- */ 
 const ctx = canvas.getContext('2d')
 console.log(ctx);
 canvas.setAttribute('height', getComputedStyle(canvas).height);
 canvas.setAttribute('width', getComputedStyle(canvas).width);
-
-// ctx.fillStyle = 'purple';
-// ctx.fillRect(0, 0, 40, 40); //top left corner
-// ctx.fillStyle = 'orange';
-// ctx.fillRect(600, 600, 40, 40); //bottom right corner
-// ctx.fillRect(0, 600, 40, 40); //bottom left corner
-// ctx.fillRect(600, 0, 40, 40); //top right corner
-// ctx.strokeStyle = 'black';
-// ctx.strokeRect(300, 300, 40, 40); //middle
 
 /* ----- CLASSES ------ */
 
@@ -56,89 +46,41 @@ class Food {
 
 /* ----- BOARD OBJECTS ------ */
 
-// const mySnake = new Subject(300,100,40,40,'black');
-// mySnake.render();
-
-
-
 const snakeFood = new Food(400,300,40,40,'orange')
 snakeFood.render();
 
-  /*----- state variables -----*/
-
-
-  /*----- cached elements  -----*/
-
 
   /*----- event listeners -----*/
-// canvas.addEventListener('click', e => {      //making sure it works. this one gives click coordinates
-//     console.log(`x: ${e.offsetX}, y:${e.offsetY}`)
-// })
-
 document.addEventListener('keydown', moveSnake);
-//button.addEventListener('click', reset);
 
+button.addEventListener('click', reset);
 
   /*----------------------------------- functions --------------------------------------*/
 
 
-//function reset () {
-    //console.log('reset button clicked');
-//render snake to default
-//     drawSnake(snake, ctx);
-//set 'direction' to null
-//      direction = null;
-// randomly generate food
-//      snakeFood.render();
-// }
-
-
-
 function reset() {
-    // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Reset the snake to its initial state
     snake = [
         { x: 300, y: 100, height: 40, width: 40, color: 'red' },
         { x: 300, y: 60, height: 40, width: 40, color: 'red' },
         { x: 300, y: 20, height: 40, width: 40, color: 'red' },
     ];
-    // Reset the direction
     direction = null;
-    // Reset the score
     points = 0;
     score.innerHTML = `Score: 0`;
-    // Randomly re-generate food
     snakeFood.x = Math.floor(Math.random() * (canvas.width - snakeFood.width));
     snakeFood.y = Math.floor(Math.random() * (canvas.height - snakeFood.height));
-    // Clear game over message
     message.innerText = '';
-    // Clear interval for automove snake
-            //clearInterval(autoMoveInterval);
-    // start auto move again
     autoMoveInterval = setInterval(autoMoveSnake, 100);
     document.addEventListener('keydown', moveSnake);
-            //gameloop();
-    // Render the initial state
-            // drawSnake(snake, ctx);
-            // snakeFood.render();
 }
 
-button.addEventListener('click', reset);
 
-
-
-const gameInterval = setInterval(gameloop, 50);
 function gameloop () {
-    //clear canvas to re-render
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // render all game objects
     drawSnake(snake, ctx);
     snakeFood.render();
-    // start the movement
-    //do game logic
     collisionDetect(snake[0].x ,snake[0].y, snakeFood);
-
 }
 
 
@@ -148,15 +90,9 @@ function drawSnake (snake, ctx){
     ctx.fillRect(segment.x, segment.y, segment.width, segment.height);
 });
 }
-//drawSnake(snake, ctx);
 
 
 function moveSnake (e){
-    //console.log(e)
-    // const speed = 40;
-    // let snakeHeadx = snake[0].x;
-    // let snakeHeady = snake[0].y;
-    // const snakeTail = snake[snake.length];
     switch(e.key) {
         case 'ArrowUp':
             if (direction === 'down' || direction === null){
@@ -200,8 +136,6 @@ function autoMoveSnake(){
     const snakeTail = snake[snake.length];
             if (direction === 'null') {
                 return;
-            //  } else if (collisionDetect === true){
-            //      clearInterval(autoMoveInterval);
             } else if (direction === 'up'){
                 let newHeadup = {x: snakeHeadx, y: snakeHeady -= speed, height: 40, width: 40, color: 'red'};
                 snake.unshift(newHeadup);
@@ -224,129 +158,7 @@ function autoMoveSnake(){
                 drawSnake(snake, ctx);
             }
 }
-// function moveSnake (e){
-//     //console.log(e)
-//     const speed = 40;
-//     let snakeHeadx = snake[0].x;
-//     let snakeHeady = snake[0].y;
-//     const snakeTail = snake[snake.length];
-//     switch(e.key) {
-//         case 'ArrowUp':
-//             if (direction === 'down'){
-//                 return;
-//             } else { 
-                
-//                     let newHeadup = {x: snakeHeadx, y: snakeHeady -= speed, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeadup);
-//                     snake.pop(snakeTail);
-//                     direction = 'up';
-//                     drawSnake(snake, ctx);
-                
-//             }
-//             break;
-//         case 'ArrowDown':
-//             if (direction === 'up'){
-//                 return;
-//             } else { 
-//                     let newHeaddown = {x: snakeHeadx, y: snakeHeady += speed, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeaddown);
-//                     snake.pop(snakeTail);
-//                     direction = 'down';
-//                     drawSnake(snake, ctx);
-//             }
-//             break;
-//         case 'ArrowRight':
-//             if (direction === 'left'){
-//                 return;
-//             } else {
-                
-//                     let newHeadright = {x: snakeHeadx += speed, y: snakeHeady, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeadright);
-//                     snake.pop(snakeTail);
-//                     direction ='right';
-//                     drawSnake(snake, ctx);
-                
-//             }
-//             break;
-//         case 'ArrowLeft':
-//             if (direction === 'right'){
-//                 return;
-//             } else { 
-                
-//                     let newHeadleft = {x: snakeHeadx -= speed, y: snakeHeady, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeadleft);
-//                     snake.pop(snakeTail);
-//                     direction = 'left';
-//                     drawSnake(snake, ctx);
-                
-//             }
-//             break;
-//         default: console.log(`${e.key} not recognized`)
-//     }
-// }
 
-// function moveSnake (e){
-//     //console.log(e)
-//     const speed = 40;
-//     let snakeHeadx = snake[0].x;
-//     let snakeHeady = snake[0].y;
-//     const snakeTail = snake[snake.length];
-//     switch(e.key) {
-//         case 'ArrowUp':
-//             if (direction === 'down'){
-//                 return;
-//             } else { 
-//                 const continueUp = setInterval(() => {
-//                     let newHeadup = {x: snakeHeadx, y: snakeHeady -= speed, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeadup);
-//                     snake.pop(snakeTail);
-//                     direction = 'up';
-//                     drawSnake(snake, ctx);
-//                 }, 500);
-//             }
-//             break;
-//         case 'ArrowDown':
-//             if (direction === 'up'){
-//                 return;
-//             } else { 
-//                 const continueDown = setInterval(() => {
-//                     let newHeaddown = {x: snakeHeadx, y: snakeHeady += speed, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeaddown);
-//                     snake.pop(snakeTail);
-//                     direction = 'down';
-//                     drawSnake(snake, ctx);
-//                 }, 500);
-//             }
-//             break;
-//         case 'ArrowRight':
-//             if (direction === 'left'){
-//                 return;
-//             } else {
-//                 const continueRight = setInterval(() => {
-//                     let newHeadright = {x: snakeHeadx += speed, y: snakeHeady, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeadright);
-//                     snake.pop(snakeTail);
-//                     direction ='right';
-//                     drawSnake(snake, ctx);
-//                 }, 500)
-//             }
-//             break;
-//         case 'ArrowLeft':
-//             if (direction === 'right'){
-//                 return;
-//             } else { 
-//                 const continueDown = setInterval(() => {
-//                     let newHeadleft = {x: snakeHeadx -= speed, y: snakeHeady, height: 40, width: 40, color: 'red'};
-//                     snake.unshift(newHeadleft);
-//                     snake.pop(snakeTail);
-//                     direction = 'left';
-//                     drawSnake(snake, ctx);
-//                 }, 500);
-//             }
-//             break;
-//         default: console.log(`${e.key} not recognized`)
-//     }
-// }
 
 function collisionDetect (snakeHeadx,snakeHeady, snakeFood) {
     snakeHeadx = snake[0].x;
@@ -403,115 +215,3 @@ function collisionDetect (snakeHeadx,snakeHeady, snakeFood) {
     }
     return false;
 }
-
-
-
-// {highScore.innerHTML= `High Score: ${highPoints}`;}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 1. Define global variables
-//    - ...
-//    - Initialize snake length to 1 (snake.length = 1)
-
-// 2. Function to initialize the game (initGame):
-//    - ...
-//    - Reset snake length to 1 (snake.length = 1)
-
-// 3. Function to check for collisions (checkCollisions):
-//    - Check if the snake's head collides with the food:
-//      - If yes, increment the score
-//      - Generate new food
-//      - Increase the snake's length by adding a new segment to the tail
-
-// 4. Function to move the snake (moveSnake):
-//    - Calculate the new head position based on the current direction
-//    - Add the new head to the beginning of the snake array
-//    - If the snake didn't eat the food, remove the tail (snake.pop())
-
-// 5. Function to generate and render food (generateFood):
-//    - Randomly generate a new food position within the canvas
-
-// 6. Function to render the game (renderGame):
-//    - Clear the canvas
-//    - Draw the snake on the canvas
-//    - Draw the food on the canvas
-//    - Display the current score and high score on the screen
-//    - If gameOver is true, display the game over screen
-
-// 7. Function to handle game over (handleGameOver):
-//    - Set gameOver to true
-//    - Update the high score if the current score is higher
-//    - Display the game over screen with the final score and high score
-//    - Remove the event listener for key input
-
-// 8. Function to restart the game (restartGame):
-//    - Reset all game variables using initGame()
-
-// 9. Function to control the game loop (gameLoop):
-//    - If gameOver is true, return and do not continue the game loop
-//    - Move the snake
-//    - Check for collisions
-//    - Render the game
-//    - Call requestAnimationFrame to continue the game loop
-
-// 10. Initialize the game when the page loads (window.addEventListener("load", initGame))
-
-// 11. Handle user clicks on the start and restart buttons:
-//     - Start button (document.getElementById("start-button").addEventListener("click", initGame))
-//     - Restart button (document.getElementById("restart-button").addEventListener("click", restartGame))
